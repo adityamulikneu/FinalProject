22c55f27b466fc226aa1ae09796c94b9035099a8
Divyesh Darji <darji.di@northeastern.edu>
2021-12-12 10:35:11 -0500

    Constants

---
+++ FinalProject/src/Constants/StringConstants.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Constants;
+
+/**
+ *
+ * @author divye
+ */
+public class StringConstants {
+    public enum Status{
+        Pending,
+        Completed,
+        GetMedications;
+    }
+}
